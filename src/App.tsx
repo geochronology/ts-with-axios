ca89341167e9537cb805c57d816188269d34ae04
geochronology <54387644+geochronology@users.noreply.github.com>
2021-03-14 20:04:39 -0700

    added error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // Source: https://medium.com/swlh/interacting-with-restful-apis-using-typescript-react-hooks-and-axios-part-1-af52920ae3e4
 import React, { useState } from 'react';
-import axios from "axios"
+import axios, { CancelTokenSource } from "axios"
 import './App.css';
 
 interface IPost {
@@ -16,34 +16,55 @@ const App: React.FC = () => {
   const [loading, setLoading]: [boolean, (loading: boolean) => void] = useState<boolean>(true);
   const [error, setError]: [string, (error: string) => void] = useState("");
 
+  // cancel token setup
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource, (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+
   React.useEffect(() => {
 
     axios
       .get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
+        cancelToken: cancelTokenSource.token,
         headers: {
           "Content-Type": "application/json"
         },
-        timeout: 3 /* in ms */
+        timeout: 3000 /* in ms */
       })
       .then(res => {
         setPosts(res.data)
         setLoading(false)
       })
       .catch(ex => {
-        const err =
-          ex.code === "ECONNABORTED"
-            ? "A timeout has occurred"
+        const err = axios.isCancel(ex)
+          ? 'Request cancelled'
+          : ex.code === 'ECONNABORTED'
+            ? 'A timeout has occurred'
             : ex.response.status === 404
-              ? "Resource not found"
-              : "An unexpected error has occurred";
+              ? 'Resource not found'
+              : 'An unexpected error has occurred';
         setError(err);
         setLoading(false);
       })
 
+    // uncomment this to simulate axios cancellation
+    cancelTokenSource.cancel("User cancelled operation");
   }, []);
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel("User cancelled operation");
+    }
+  };
 
   return (
     <div className="App">
+
+      {loading && (
+        <button onClick={handleCancelClick}>Cancel</button>
+      )}
+
       <ul className="posts">
         {posts.map((post) => (
           <li key={post.id}>
